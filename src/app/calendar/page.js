c405151c5795750a1a6ad3ee09a76d9ee4da2595
ruirun <indevruis@gmail.com>
2023-07-01 20:08:40 +0900

    Style: calendar css 적용
    Header min-width 추가

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,15 +1,18 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "./calendar.css";
 
 export default function calendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div className='div'>
-      <Calendar locale="ko" onChange={onChange} value={value} />
+    <div className='react-calendar-background'>
+      <h3>Calendar</h3>
+      <div className='calendar-container'>
+        <Calendar locale="en" onChange={onChange} value={value} />
+      </div>
     </div>
   );
 }
