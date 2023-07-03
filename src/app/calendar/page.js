03e62a43cdacbe0a365907e9e44cd48fd491163c
ruirun <indevruis@gmail.com>
2023-07-03 13:26:40 +0900

    Feat: Google Calendar API 불러오기

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,11 +1,29 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "./calendar.css";
 
 export default function calendar() {
   const [value, onChange] = useState(new Date());
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_HOLIDAY_API;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/calendar/v3/calendars/ko.south_korea%23holiday%40group.v.calendar.google.com/events?key=${API_KEY}`
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json);
+          });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="react-calendar-background">
