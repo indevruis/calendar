177bdb8e27201279a2d1052c88e8bad4fb48f219
ruirun <indevruis@gmail.com>
2023-08-14 10:58:52 +0900

    Feat: calendar 수정
    css 파일을 public 폴더 안으로 이동
    moment -> dayjs 사용
    css 변경 및 page.js 변경

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,66 +1,33 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useState, useEffect, useCallback } from "react";
+import Navbar from "../components/Navbar";
 import Calendar from "react-calendar";
-import moment from "moment";
-import "./calendar.css";
+import dayjs from "dayjs";
+import "public/calendar.css";
 
-export default function calendar() {
+const calendar = () => {
   const [value, onChange] = useState(new Date());
-  const [holidays, setHolidays] = useState([]);
-  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_HOLIDAY_API;
-
-  useEffect(() => {
-    getHolidayApi();
-  }, []);
-
-  const getHolidayApi = () => {
-    try {
-      fetch(
-        `https://www.googleapis.com/calendar/v3/calendars/ko.south_korea%23holiday%40group.v.calendar.google.com/events?key=${API_KEY}`
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          setHolidays(json.items);
-        });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
   return (
-    <div className="react-calendar-background">
-      <div className="calendar-container">
+    <div className="w-h-100">
+      <Navbar />
+      <div className="w-h-100 flexbox" style={{ paddingTop: "65px" }}>
         <Calendar
           locale="en"
           onChange={onChange}
           value={value}
-          calendarType={"US"}
-          tileContent={({ date, view }) => {
-            const find = holidays.find(
-              (holiday) =>
-                holiday.start.date === moment(date).format("YYYY-MM-DD")
-            );
-            return find && view === "month" ? (
-              <div className="holidays">{find.summary}</div>
-            ) : (
-              ""
-            );
-          }}
+          calendarType="gregory"
           tileClassName={({ date }) => {
-            const find = holidays.find(
-              (holiday) =>
-                holiday.start.date === moment(date).format("YYYY-MM-DD")
-            );
-            const dayOfTheWeek = date.getDay();
-            return find || (dayOfTheWeek === 0)
-              ? "react-calendar_tile--holidays"
-              : dayOfTheWeek === 6
-              ? "react-calendar_tile--saturday"
+            const findSat = date.getDay();
+            return findSat === 6
+              ? "react-calendar__month-view__days__day--saturday"
               : "";
           }}
         />
       </div>
     </div>
   );
-}
+};
+
+export default calendar;
