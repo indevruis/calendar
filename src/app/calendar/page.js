02db61b0da3b04cc3167f2d6c75c6d6de1b68649
ruirun <indevruis@gmail.com>
2023-08-23 10:33:14 +0900

    Feat: Get open API holiday

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,22 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
 import Calendar from "./Components/CalenderContent";
 import List from "./Components/List";
 
 const calendar = () => {
+  const [year, setYear] = useState("2023");
+  const [holidays, setHolidays] = useState([]);
   const [list, setList] = useState({
     date: "",
     isOpen: false,
     tasks: ["할일1", "할일2"],
   });
 
+  const handleGetHolidayAPI = async () => {
+    try {
+      const response = await fetch(`/api/calendar/holiday?year=${year}`, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => setHolidays(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetHolidayAPI();
+  }, []);
+
   return (
     <div className="w-full h-full">
       <Navbar />
       <div className="w-full h-full flex justify-center items-center pt-[65px]">
-        <Calendar list={list} setList={setList} />
+        <Calendar
+          list={list}
+          setList={setList}
+          holidays={holidays}
+          setYear={setYear}
+        />
         {list.isOpen && <List list={list} />}
       </div>
     </div>
---
+++ src/pages/api/calendar/holiday.js
@@ -0,0 +1,35 @@
+import dayjs from "dayjs";
+
+const NEXT_PUBLIC_HOLIDAY_API_ENCODING =
+  process.env.NEXT_PUBLIC_HOLIDAY_API_ENCODING;
+
+const convertHolidaysToData = (holidays) => {
+  return holidays.map((holiday) => {
+    const customDate = dayjs(holiday.locdate.toString(), "YYYYMMDD").format(
+      "YYYY-MM-DD"
+    );
+    return {
+      title: holiday.dateName,
+      date: customDate,
+    };
+  });
+};
+
+const holidayAPI = async (req, res) => {
+  const year = req.query.year;
+
+  if (req.method === "GET") {
+    try {
+      const response = await fetch(
+        `http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?solYear=${year}&numOfRows=30&ServiceKey=${NEXT_PUBLIC_HOLIDAY_API_ENCODING}&_type=json`
+      );
+      const data = await response.json();
+      const holidays = convertHolidaysToData(data.response.body.items.item);
+      res.status(200).json(holidays);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
+
+export default holidayAPI;
