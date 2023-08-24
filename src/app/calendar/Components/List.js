6866cffc2b64582348b8b5563e1172916d1c0e94
ruirun <indevruis@gmail.com>
2023-08-24 10:02:23 +0900

    Style: Change the layout of the list

--- src/app/calendar/Components/List.js
+++ src/app/calendar/Components/List.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import styles from "/public/List.module.css";
+import styles from "/public/css/List.module.css";
 
 const List = ({ list }) => {
   const nowDate = dayjs(list.date).format("YYYY.MM.DD");
@@ -7,9 +7,29 @@ const List = ({ list }) => {
     <div className={`${styles.background}`}>
       <div className={`${styles.clickedDay}`}>{nowDate}</div>
       <div className={`${styles.tasks}`}>
+          <span className={`${styles.taskTitle}`}>Daily</span>
         {list.tasks.map((task, i) => (
-          <div key={i} className="task">
-            {task}
+          <div key={i} className="flex m-[1px] items-center">
+            <span className={`${styles.checkBox}`}></span>
+            <span className={`${styles.task}`}>{task}</span>
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.tasks}`}>
+          <span className={`${styles.taskTitle}`}>weekly</span>
+        {list.tasks.map((task, i) => (
+          <div key={i} className="flex m-[1px] items-center">
+            <span className={`${styles.checkBox}`}></span>
+            <span className={`${styles.task}`}>{task}</span>
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.tasks}`}>
+          <span className={`${styles.taskTitle}`}>monthly</span>
+        {list.tasks.map((task, i) => (
+          <div key={i} className="flex m-[1px] items-center">
+            <span className={`${styles.checkBox}`}></span>
+            <span className={`${styles.task}`}>{task}</span>
           </div>
         ))}
       </div>
