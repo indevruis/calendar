a0dcb5a98327b707346af05a0428ff4702bb82e3
ruirun <indevruis@gmail.com>
2023-09-03 19:48:29 +0900

    Fix: Layout 수정

--- src/app/todo/layout.js
+++ src/app/todo/layout.js
@@ -2,11 +2,9 @@ import Navbar from "../Components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+    </>
   );
 }
